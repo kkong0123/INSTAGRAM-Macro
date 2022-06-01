6b6c335e85084ccf36c037a17ee1ecb39fc12803
공상혁 <kkong0123@naver.com>
2022-06-01 21:51:07 +0900

    [UPDATE] 이미 팔로우 한 계정은 건너뛰기 기능 추가 / 시간 경과 함수 추가 / 언팔로우 함수의 스크롤 기능 업데이트 / 맞팔 확인 후 팔로우 취소 기능 추가

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-from curses import KEY_ENTER
+from selenium import webdriver
 from random import randint
 import random
-from selenium import webdriver
 import time
 import sys
 import os
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 def craw_main():
+        global instagram_id
+        global instagram_pwd
+        global search_tag
         global driver
+        global start_time
+
+##################################################
+
+        instagram_id = ""
+        instagram_pwd = ""
+        search_tag = ""
+
+##################################################
+
+        start_time = time.time() # 시작시간
         options = webdriver.ChromeOptions()
-        # options.add_argument('--no-sandbox')
-        # options.add_argument('--disable-dev-shm-usage')
-        # options.add_argument("--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25")
-        # options.add_argument("--start-maximized")
-        # 창 숨기는 옵션 추가
         # options.add_argument("headless")
 
         if getattr(sys, 'frozen', False):
@@ -30,25 +34,29 @@ def craw_main():
         driver.get(url)
         driver.implicitly_wait(15)
 
-        login() #로그인 함수 호출
-        search()
-        # unfollow()
+        login() # 로그인 함수 호출
+        # search() # 좋아요, 댓글, 팔로우 매크로 함수 호출
+        unfollow() # 언팔로우 매크로 함수 호출
 
 def login(): # 로그인 함수
-    driver.find_element_by_css_selector(".-MzZI:nth-child(1) .zyHYP").send_keys("")
+    driver.find_element_by_css_selector(".-MzZI:nth-child(1) .zyHYP").send_keys(instagram_id)
     print("id 입력 완료")
     time.sleep(random.uniform(2,5))
-    driver.find_element_by_css_selector(".-MzZI+ .-MzZI .zyHYP").send_keys("")
-    print("password 입력 완료")
+    driver.find_element_by_css_selector(".-MzZI+ .-MzZI .zyHYP").send_keys(instagram_pwd)
+    print("password 입력 완료") 
     time.sleep(random.uniform(2,5))
     driver.find_element_by_css_selector(".-MzZI+ .DhRcB").click()
     driver.implicitly_wait(15)
     time.sleep(5)
     print("로그인 성공")
 
+###
+    print("== 경과시간 ==")
+    print(elapsedTime())
+###
 
 def search():
-    url = 'https://www.instagram.com/explore/tags/' + str("")
+    url = 'https://www.instagram.com/explore/tags/' + search_tag
     driver.get(url)
     driver.implicitly_wait(15)
     print("해시태그 검색 중..")
@@ -69,8 +77,8 @@ def search():
         driver.implicitly_wait(15)
         time.sleep(5)
 
-    for i in range(300):
-
+    for i in range(400):
+        print(elapsedTime())
         if (i+1) % 15 == 0: # 쉬어가기
             time.sleep(random.uniform(7,15))
             print("쉬어가기, 누적 반복수: {}".format(i))
@@ -87,94 +95,98 @@ def search():
             "게시물 잘보고갑니당! 맞팔해요 :D"
         ]
         random_message = random_message[randint(0,7)]
+        follow_text = driver.find_element_by_css_selector(".bY2yH .T0kll").text
 
-        #####
-        time.sleep(random.uniform(60,80))
-        #####
+        if follow_text == "팔로우":
 
-        try:
-            driver.find_element_by_css_selector(".fr66n .wpO6b").click() # 좋아요 누르기
-            print("{}번째 좋아요".format(i+1))
-            time.sleep(random.uniform(3,6))
-        except Exception:
-                driver.find_element_by_css_selector(".l8mY4 .wpO6b").click()
-                print("로딩 오류 발생, 다음 게시물 이동 중..\n =======================================")
-                driver.implicitly_wait(30)
-                time.sleep(random.uniform(3,6))
+            #####
+            time.sleep(random.uniform(60,80))
+            #####
 
-                #####
-                time.sleep(random.uniform(60,80))
-                #####
-
+            try:
                 driver.find_element_by_css_selector(".fr66n .wpO6b").click() # 좋아요 누르기
                 print("{}번째 좋아요".format(i+1))
                 time.sleep(random.uniform(3,6))
-        
-        if (i+1) % 9 == 0: # 쉬어가기
-            time.sleep(random.uniform(10,20))
-            print("쉬어가기, 누적 반복수: {}".format(i))
+            except Exception:
+                    driver.find_element_by_css_selector(".l8mY4 .wpO6b").click()
+                    print("로딩 오류 발생, 다음 게시물 이동 중..\n =======================================")
+                    driver.implicitly_wait(30)
+                    time.sleep(random.uniform(3,6))
 
-        try: # 댓글 남기기
-            driver.find_element_by_css_selector(".X7cDz").click()
-            time.sleep(random.uniform(1,5))
-            driver.find_element_by_css_selector(".Ypffh").send_keys(random_message)
-            time.sleep(random.uniform(1,5))
+                    #####
+                    time.sleep(random.uniform(60,80))
+                    #####
 
-            ######
+                    driver.find_element_by_css_selector(".fr66n .wpO6b").click() # 좋아요 누르기
+                    print("{}번째 좋아요".format(i+1))
+                    time.sleep(random.uniform(3,6))
+            
+            if (i+1) % 9 == 0: # 쉬어가기
+                time.sleep(random.uniform(10,20))
+                print("쉬어가기, 누적 반복수: {}".format(i))
+
+            try: # 댓글 남기기
+                driver.find_element_by_css_selector(".X7cDz").click()
+                time.sleep(random.uniform(1,5))
+                driver.find_element_by_css_selector(".Ypffh").send_keys(random_message)
+                time.sleep(random.uniform(1,5))
+
+                ######
+                time.sleep(random.uniform(60,80))
+                ######
+
+                driver.find_element_by_css_selector(".gtFbE").click()
+                print("{0}번째 댓글입력: {1}".format(i+1, random_message))
+                driver.implicitly_wait(15)
+                time.sleep(random.uniform(4,6))
+            except Exception:
+                pass
+
+            # 팔로우 하기
+            
+            #####
             time.sleep(random.uniform(60,80))
-            ######
+            #####
 
-            driver.find_element_by_css_selector(".gtFbE").click()
-            print("{0}번째 댓글입력: {1}".format(i+1, random_message))
-            driver.implicitly_wait(15)
-            time.sleep(random.uniform(4,6))
-        except Exception:
-            pass
+            driver.find_element_by_css_selector(".bY2yH .T0kll").click()
+            print("{0}번째 팔로우".format(i+1))
+            time.sleep(random.uniform(7,12))
 
-        # 팔로우 하기
-        
-        #####
-        time.sleep(random.uniform(60,80))
-        #####
-
-        driver.find_element_by_css_selector(".bY2yH .T0kll").click()
-        print("{0}번째 팔로우".format(i+1))
-        time.sleep(random.uniform(7,12))
-
-        # 다음 버튼 누르기
-        try:
+            # 다음 버튼 누르기
             driver.find_element_by_css_selector(".l8mY4 .wpO6b").click()
             print("다음 게시물 이동 중..\n =======================================")
             driver.implicitly_wait(30)
             time.sleep(random.uniform(3,6))
 
-        except Exception: # 팔로잉 한 사람을 만났을 때
-            driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.-Cab_").click()
-            print("팔로우 취소 완료")
-            driver.implicitly_wait(10)
-            time.sleep(random.uniform(2,6))
+        else:
+            print("[이미 팔로우한 유저입니다]\n{0}번째 작업 건너뛰기".format(i+1))
+            time.sleep(random.uniform(3,6))
             driver.find_element_by_css_selector(".l8mY4 .wpO6b").click()
             print("다음 게시물 이동 중..\n =======================================")
-            driver.implicitly_wait(10)
+            driver.implicitly_wait(30)
             time.sleep(random.uniform(3,6))
+            
+
 
 def unfollow():
-    url = 'https://www.instagram.com/' + str("") 
+    # 팔로워 정보 수집
+    url = 'https://www.instagram.com/' + instagram_id
     driver.get(url)
     time.sleep(3)
     driver.find_element_by_css_selector(".Y8-fY:nth-child(2) .g47SY").click()
     time.sleep(3)
 
-    pop_up_window = WebDriverWait(
-        driver, 2).until(EC.element_to_be_clickable(
-            (By.XPATH, "//div[@class='isgrP']")))
-    
-    # 팝업창 스크롤
-    for i in range(20):
-        driver.execute_script(
-            'arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', 
-        pop_up_window)
-        time.sleep(3)
+    scroll_box = driver.find_element_by_css_selector(".isgrP")
+    last_ht, ht = 0, 1
+    while last_ht != ht:
+        last_ht = ht
+        driver.implicitly_wait(30)
+        time.sleep(4)            
+
+        ht = driver.execute_script("""
+            arguments[0].scrollTo(0, arguments[0].scrollHeight); 
+            return arguments[0].scrollHeight;
+            """, scroll_box)
 
     follower_list = []
     follower = driver.find_elements_by_css_selector("._0imsa .T0kll")
@@ -185,23 +197,26 @@ def unfollow():
     print(follower_list)
     print(len(follower_list))
     print('=='*20)
-###########
-    url = 'https://www.instagram.com/' + str("") 
+
+    # 팔로잉 정보 수집
+    url = 'https://www.instagram.com/' + instagram_id 
     driver.get(url)
     time.sleep(3)
     driver.find_element_by_css_selector(".Y8-fY~ .Y8-fY+ .Y8-fY .g47SY").click()
     time.sleep(3)
 
-    pop_up_window = WebDriverWait(
-        driver, 2).until(EC.element_to_be_clickable(
-            (By.XPATH, "//div[@class='isgrP']")))
-    
-    # Scroll till Followers list is there
-    for i in range(30):
-        driver.execute_script(
-            'arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', 
-        pop_up_window)
-        time.sleep(1)
+    # 팔로워 팝업창에서 아래까지 스크롤 다운하기
+    scroll_box = driver.find_element_by_css_selector(".isgrP")
+    last_ht, ht = 0, 1
+    while last_ht != ht:
+        last_ht = ht
+        driver.implicitly_wait(30)
+        time.sleep(4)
+
+        ht = driver.execute_script("""
+            arguments[0].scrollTo(0, arguments[0].scrollHeight); 
+            return arguments[0].scrollHeight;
+            """, scroll_box)
 
     following_list = []
     following = driver.find_elements_by_css_selector("._0imsa .T0kll")
@@ -213,9 +228,69 @@ def unfollow():
     print(len(following_list))
     print('=='*20)
 
-    following_sub_follower = [x for x in following if x not in follower]
+    following_sub_follower = [x for x in following_list if x not in follower_list]
     print("맞팔 안됨")
+
     print(following_sub_follower)
     print(len(following_sub_follower))
+    print("======= 총 경과시간 =======")
+    print(elapsedTime())
+
+    n = 0
+    # 맞팔 안된 사람 언팔하기
+    for uf in following_sub_follower:
+        url = 'https://www.instagram.com/' + str(uf) 
+        driver.get(url)
+        driver.implicitly_wait(30)
+        try:
+            #####
+            time.sleep(random.uniform(60,80))
+            #####
+
+            n += 1
+            time.sleep(random.uniform(5,7))
+            driver.find_element_by_css_selector(".T0kll ._4EzTm").click()
+            time.sleep(random.uniform(3,6))
+
+            #####
+            time.sleep(random.uniform(60,80))
+            #####
+
+            driver.find_element_by_css_selector(".-Cab_").click()
+            print("{0}/{1} 팔로우 취소 완료".format(n, len(following_sub_follower)))
+            print("======= 총 경과시간 =======")
+            print(elapsedTime())
+            time.sleep(random.uniform(3,6))
+        except Exception:
+            ##### 매크로 경고 받았을 때 -> 오랫동안 대기
+            time.sleep(random.uniform(600,800))
+            #####
+            pass
+
+        
+
+def elapsedTime(): # 경과시간 정보 함수
+    elapsed_time = time.time() - start_time
+    m, s = divmod(elapsed_time, 60)
+    h, m = divmod(m, 60)
+    d, h = divmod(h, 24)
+    if d > 0:
+        dtime = str(int(d)) + "일 "
+    else: 
+        dtime = ""
+        
+    if h > 0:
+        htime = str(int(h)) + "시간 "
+    else:
+        htime = ""
+        
+    if m > 0:
+        mtime = str(int(m)) + "분 "
+    else:
+        mtime = ""     
+
+    strTime = dtime + htime + mtime + str(int(s)) + "초"    
+    return strTime 
+
 
 craw_main()
