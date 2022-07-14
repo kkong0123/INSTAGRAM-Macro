ce0a05684630dd6c284aebcb40aba48c64da0667
공상혁 <kkong0123@naver.com>
2022-07-14 12:53:52 +0900

    [UPDATE]

--- main_gui.py
+++ main_gui.py
@@ -9,6 +9,7 @@ import sys
 import os
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QThread
 
 #UI파일 연결
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -26,7 +27,7 @@ class WindowClass(QMainWindow, form_class) :
         global textBrowser
         textBrowser = self.textBrowser
 
-        self.setWindowTitle('Instagram Bot v1.0.1')
+        self.setWindowTitle('Instagram Bot v1.0.2')
 
         self.start_pushButton.clicked.connect(self.main)
         self.pushButton_add.clicked.connect(self.appendList)
@@ -63,23 +64,23 @@ class WindowClass(QMainWindow, form_class) :
 ##################################################
 
         start_time = time.time() # 시작시간
-        login()
-        search()
+        WindowClass.login(self)
+        WindowClass.search(self)
 
         global i_cnt
         for i_cnt in range(int(repeat_cnt)):
             try:
                 follow_text = driver.find_element_by_css_selector("._aar2 ._aade").text
                 if follow_text == "팔로우":
-                    self.checkCnt()
-                    self.likeEnable()
-                    self.commentEnable()
-                    self.followEnable()
+                    WindowClass.checkCnt(self)
+                    WindowClass.likeEnable(self)
+                    WindowClass.commentEnable(self)
+                    WindowClass.followEnable(self)
                     # 다음 버튼 누르기
                     driver.find_element_by_css_selector("._aaqg ._abl-").click()
                     print('[' + time.strftime('%H:%M:%S') + ']' ,"다음 게시물 이동 중..\n =======================================")
                     print("======= 총 경과시간 =======")
-                    print(elapsedTime())
+                    print(WindowClass.elapsedTime(self))
                     driver.implicitly_wait(30)
                     time.sleep(random.uniform(3,6))
 
@@ -89,7 +90,7 @@ class WindowClass(QMainWindow, form_class) :
                     driver.find_element_by_css_selector("._aaqg ._abl-").click()
                     print('[' + time.strftime('%H:%M:%S') + ']' ,"다음 게시물 이동 중..\n =======================================")
                     print("======= 총 경과시간 =======")
-                    print(elapsedTime())            
+                    print(WindowClass.elapsedTime(self))            
                     driver.implicitly_wait(30)
                     time.sleep(random.uniform(3,6))
 
@@ -97,7 +98,7 @@ class WindowClass(QMainWindow, form_class) :
                 time.sleep(random.uniform(3,6))
                 driver.find_element_by_css_selector("._aaqg ._abl-").click()
                 print('[' + time.strftime('%H:%M:%S') + ']' ,"로딩 오류 발생, 다음 게시물 이동 중..\n =======================================")
-                print("[오류 메시지]", traceback.format_exc())
+                print("[오류 메시지]", traceback.format_excf())
                 driver.implicitly_wait(30)   
 
                 #####
@@ -128,15 +129,15 @@ class WindowClass(QMainWindow, form_class) :
 
     def likeEnable(self):
         if self.checkBox_like.isChecked():
-            like()
+            WindowClass.like(self)
             
     def commentEnable(self):
         if self.checkBox_comment.isChecked():
-            comment()
+            WindowClass.comment(self)
 
     def followEnable(self):
         if self.checkBox_follow.isChecked():
-            follow()
+            WindowClass.follow(self)
     
     def checkCnt(self):
         global likeStat
@@ -166,148 +167,137 @@ class WindowClass(QMainWindow, form_class) :
         elif self.checkBox_follow.isChecked():
             followStat = 1
         
-def logMsg(msg):
-    print(msg)
-    textBrowser.append(msg)
-    QApplication.processEvents()
+    def logMsg(self, msg):
+        print(msg)
+        self.textBrowser.append(msg)
+        QApplication.processEvents()
 
-def login():
-    global driver
-    if getattr(sys, 'frozen', False):
-        chromedriver_path = os.path.join(sys._MEIPASS, "./chromedriver")
-        driver = webdriver.Chrome(chromedriver_path)
-    else:
-        driver = webdriver.Chrome('./chromedriver')
+    def login(self):
+        global driver
+        if getattr(sys, 'frozen', False):
+            chromedriver_path = os.path.join(sys._MEIPASS, "./chromedriver")
+            driver = webdriver.Chrome(chromedriver_path)
+        else:
+            driver = webdriver.Chrome('./chromedriver')
 
-    url = 'https://www.instagram.com/accounts/login/'   
-    driver.get(url)
-    driver.implicitly_wait(15)
-    print(elapsedTime())
+        url = 'https://www.instagram.com/accounts/login/'   
+        driver.get(url)
+        driver.implicitly_wait(15)
+        print(WindowClass.elapsedTime(self))
 
-    driver.find_element_by_css_selector(".-MzZI:nth-child(1) .zyHYP").send_keys(instagram_id)
+        driver.find_element_by_css_selector(".-MzZI:nth-child(1) .zyHYP").send_keys(instagram_id)
 
-    logMsg('[' + time.strftime('%H:%M:%S') + '] ' + "id 입력 완료")
-    # QApplication.processEvents()
+        WindowClass.logMsg(self, '[' + time.strftime('%H:%M:%S') + '] ' + "id 입력 완료")
+        # QApplication.processEvents()
 
-    time.sleep(random.uniform(10,15))
-    driver.find_element_by_css_selector(".-MzZI+ .-MzZI .zyHYP").send_keys(instagram_pwd)
+        time.sleep(random.uniform(10,15))
+        driver.find_element_by_css_selector(".-MzZI+ .-MzZI .zyHYP").send_keys(instagram_pwd)
 
-    logMsg('[' + time.strftime('%H:%M:%S') + '] ' + "password 입력 완료")
+        WindowClass.logMsg(self, '[' + time.strftime('%H:%M:%S') + '] ' + "password 입력 완료")
 
-    time.sleep(random.uniform(5,10))
-    driver.find_element_by_css_selector(".-MzZI+ .DhRcB").click()
-    driver.implicitly_wait(15)
-    time.sleep(5)
-
-    logMsg('[' + time.strftime('%H:%M:%S') + '] ' + "로그인 성공")
-    QApplication.processEvents()
-
-    print("== 경과시간 ==")
-    print(elapsedTime())
-    
-def search():
-    url = 'https://www.instagram.com/explore/tags/' + search_tag
-    driver.get(url)
-    driver.implicitly_wait(15)
-    logMsg('[' + time.strftime('%H:%M:%S') + '] ' + "해시태그 검색 중..")
-    time.sleep(10)
-
-    pic_list = driver.find_elements_by_css_selector("._aagw , ._aanf:nth-child(1) ._a6hd")
-    pic_list = list(pic_list)
-    driver.implicitly_wait(15)
-    time.sleep(5)
-    pic_list[0].click()
-    driver.implicitly_wait(15)
-    time.sleep(5)
-
-    # 인기게시물 건너뛰기
-    for i in range(9):
-        driver.find_element_by_css_selector("._aaqg ._abl-").click()
-        logMsg('[' + time.strftime('%H:%M:%S') + '] ' + "{0}번째 인기 게시물 건너뛰기".format(i+1))
+        time.sleep(random.uniform(5,10))
+        driver.find_element_by_css_selector(".-MzZI+ .DhRcB").click()
         driver.implicitly_wait(15)
         time.sleep(5)
 
-def like():
-    delayTime()
-    driver.find_element_by_css_selector("._aamw ._abl-").click() # 좋아요 누르기
-    logMsg('[' + time.strftime('%H:%M:%S') + '] ' + "{}번째 좋아요".format(i_cnt+1))
-    time.sleep(random.uniform(3,6))
+        WindowClass.logMsg(self, '[' + time.strftime('%H:%M:%S') + '] ' + "로그인 성공")
+        QApplication.processEvents()
 
-def comment():
-    driver.find_element_by_css_selector("._aaoc").click()
-    time.sleep(random.uniform(5,10))
+        print("== 경과시간 ==")
+        print(WindowClass.elapsedTime(self))
+    
+    def search(self):
+        url = 'https://www.instagram.com/explore/tags/' + search_tag
+        driver.get(url)
+        driver.implicitly_wait(15)
+        WindowClass.logMsg(self, '[' + time.strftime('%H:%M:%S') + '] ' + "해시태그 검색 중..")
+        time.sleep(10)
 
-    random_message = randomMessage() # 랜덤메시지 함수 리턴값 가져오기
+        pic_list = driver.find_elements_by_css_selector("._aagw , ._aanf:nth-child(1) ._a6hd")
+        pic_list = list(pic_list)
+        driver.implicitly_wait(15)
+        time.sleep(5)
+        pic_list[0].click()
+        driver.implicitly_wait(15)
+        time.sleep(5)
 
-    driver.find_element_by_css_selector("._aaoc").send_keys(random_message)
+        # 인기게시물 건너뛰기
+        for i in range(9):
+            driver.find_element_by_css_selector("._aaqg ._abl-").click()
+            WindowClass.logMsg(self, '[' + time.strftime('%H:%M:%S') + '] ' + "{0}번째 인기 게시물 건너뛰기".format(i+1))
+            driver.implicitly_wait(15)
+            time.sleep(5)
 
-    delayTime()
+    def like(self):
+        WindowClass.delayTime(self)
+        driver.find_element_by_css_selector("._aamw ._abl-").click() # 좋아요 누르기
+        WindowClass.logMsg(self, '[' + time.strftime('%H:%M:%S') + '] ' + "{}번째 좋아요".format(i_cnt+1))
+        time.sleep(random.uniform(3,6))
 
-    driver.find_element_by_css_selector("._aad0").click()
-    logMsg('[' + time.strftime('%H:%M:%S') + '] ' + "{0}번째 댓글입력: {1}".format(i_cnt+1, random_message))
-    driver.implicitly_wait(15)
-    time.sleep(random.uniform(4,6))
+    def comment(self):
+        driver.find_element_by_css_selector("._aaoc").click()
+        time.sleep(random.uniform(5,10))
 
-def follow():
-    delayTime()
-    driver.find_element_by_css_selector("._aar2 ._aade").click()
-    logMsg('[' + time.strftime('%H:%M:%S') + '] ' + "{0}번째 팔로우".format(i_cnt+1))
-    time.sleep(random.uniform(7,12))
+        random_message = WindowClass.randomMessage(self) # 랜덤메시지 함수 리턴값 가져오기
 
-def randomMessage():
-    # random_message_lst = [  
-    # "잘 보고갑니다! : )",
-    # "피드 잘 보고갑니다! :D",
-    # "피드 구경 잘 하고갑니다! : ) ",
-    # "게시물 잘보고갑니당! :)",
-    # "잘 보고갑니당! 맞팔해요!:D",
-    # "피드 구경 잘 하고 갑니다! 맞팔해용 :)",
-    # "피드가 너무 예뻐요 :) 맞팔해요!",
-    # "게시물 잘보고갑니당! 맞팔해요 :D"
-    # ]
-    if len(random_message_lst) == 1:
-        random_message = random_message_lst[0]
-    else:
-        random_message = random_message_lst[randint(0,len(random_message_lst)-1)]
-    return random_message
+        driver.find_element_by_css_selector("._aaoc").send_keys(random_message)
 
-def delayTime():
-    print(likeStat)
-    print(commentStat)
-    print(followStat)
-    if likeStat + commentStat + followStat == 3:
-        print("3개 체크")
-        time.sleep(random.uniform(int(delay_time) / 3, int(delay_time) / 3 + 30))
-    elif likeStat + commentStat + followStat == 2:
-        print("2개 체크")      
-        time.sleep(random.uniform(int(delay_time) / 2, int(delay_time) / 2 + 30))
-    else:
-        print("1개 체크")
-        time.sleep(random.uniform(int(delay_time), int(delay_time) + 30))
+        WindowClass.delayTime(self)
 
-def elapsedTime(): # 경과시간 정보 함수
-    elapsed_time = time.time() - start_time
-    m, s = divmod(elapsed_time, 60)
-    h, m = divmod(m, 60)
-    d, h = divmod(h, 24)
-    if d > 0:
-        dtime = str(int(d)) + "일 "
-    else: 
-        dtime = ""
-    if h > 0:
-        htime = str(int(h)) + "시간 "
-    else:
-        htime = ""
-    if m > 0:
-        mtime = str(int(m)) + "분 "
-    else:
-        mtime = ""     
-    strTime = dtime + htime + mtime + str(int(s)) + "초"    
-    return strTime 
+        driver.find_element_by_css_selector("._aad0").click()
+        WindowClass.logMsg(self, '[' + time.strftime('%H:%M:%S') + '] ' + "{0}번째 댓글입력: {1}".format(i_cnt+1, random_message))
+        driver.implicitly_wait(15)
+        time.sleep(random.uniform(4,6))
+
+    def follow(self):
+        WindowClass.delayTime(self)
+        driver.find_element_by_css_selector("._aar2 ._aade").click()
+        WindowClass.logMsg(self, '[' + time.strftime('%H:%M:%S') + '] ' + "{0}번째 팔로우".format(i_cnt+1))
+        time.sleep(random.uniform(7,12))
+
+    def randomMessage(self):
+        if len(random_message_lst) == 1:
+            random_message = random_message_lst[0]
+        else:
+            random_message = random_message_lst[randint(0,len(random_message_lst)-1)]
+        return random_message
+
+    def delayTime(self):
+        print(likeStat)
+        print(commentStat)
+        print(followStat)
+        if likeStat + commentStat + followStat == 3:
+            print("3개 체크")
+            time.sleep(random.uniform(int(delay_time) / 3, int(delay_time) / 3 + 30))
+        elif likeStat + commentStat + followStat == 2:
+            print("2개 체크")      
+            time.sleep(random.uniform(int(delay_time) / 2, int(delay_time) / 2 + 30))
+        else:
+            print("1개 체크")
+            time.sleep(random.uniform(int(delay_time), int(delay_time) + 30))
+
+    def elapsedTime(self): # 경과시간 정보 함수
+        elapsed_time = time.time() - start_time
+        m, s = divmod(elapsed_time, 60)
+        h, m = divmod(m, 60)
+        d, h = divmod(h, 24)
+        if d > 0:
+            dtime = str(int(d)) + "일 "
+        else: 
+            dtime = ""
+        if h > 0:
+            htime = str(int(h)) + "시간 "
+        else:
+            htime = ""
+        if m > 0:
+            mtime = str(int(m)) + "분 "
+        else:
+            mtime = ""     
+        strTime = dtime + htime + mtime + str(int(s)) + "초"    
+        return strTime 
 
 if __name__ == "__main__" :        
     app = QApplication(sys.argv)
     myWindow = WindowClass() 
     myWindow.show()
     app.exec_()
-
